1a78e784c470db58ecc8a9c151b98811d7c0c42d
Francesco Pietro Rizzello <francescorizzello94@gmail.com>
2022-08-12 17:36:32 +0200

    add Profile container

---
+++ src/containers/Profile.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+
+function Profile({ gitHubName}) {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    async function fetchData() {
+      const profile = await fetch("https://api.github.com/users/holoplot");
+      const information = await profile.json();
+
+      if (information) {
+        setData(information);
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, [gitHubName]);
+
+  return (
+    <div className='Profile-container'>
+      <h2>Holoplot - General Overview</h2>
+      {
+        loading
+          ?
+          (<span>Loading...</span>)
+          :
+          (
+        <div>
+          <img
+            className='Profile-avatar'
+            src={data.avatar_url}
+            alt={data.name}
+          />
+          <List items={items} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Profile;
