a7971c99c1000345dcbf98591b0bd8f2fd040ac7
Francesco Pietro Rizzello <francescorizzello94@gmail.com>
2022-08-14 20:18:51 +0200

    clean up data only retain repo data remove general overview

--- src/containers/Profile/Profile.js
+++ src/containers/Profile/Profile.js
@@ -1,23 +1,9 @@
 import React, { useEffect, useState } from "react";
-import Link from "../../components/Link/Link";
-import List from "../../components/List/List";
 import './Profile.css';
 
 function Profile({ gitHubName="holoplot"}) {
   const [data, setData] = useState({});
   const [loading, setLoading] = useState(true);
-
-  const items = [{
-    field: 'html_url',
-    value: <Link url={data.html_url} title={ data.html_url} />,
-  },
-    {
-      field: 'repos_url',
-      value: <Link url={data.repos_url} title={ data.repos_url} />,
-    },
-    { field: 'name', value: data.name },
-    { field: 'location', value: data.location },
-  ]
 
 
   useEffect(() => {
@@ -35,7 +21,6 @@ function Profile({ gitHubName="holoplot"}) {
 
   return (
     <div className='Profile-container'>
-      <h2>Holoplot - General Overview</h2>
       {
         loading
           ?
@@ -48,7 +33,6 @@ function Profile({ gitHubName="holoplot"}) {
             src={data.avatar_url}
             alt={data.name}
           />
-          <List items={items} />
         </div>
       )}
     </div>
